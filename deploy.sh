9e079d899e85c01df121ea7c2313f98e9dce7bbe
Matt <matthewd102@hotmail.com>
2021-02-17 12:07:26 +0000

    Tidy up Travis file

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+cd src/LevelDBDumper
+export GOOS="linux"
+go build -o LevelDBDumper
+export GOOS="darwin"
+go build -o LevelDBDumper.app
+export GOOS="windows"
+go build -o LevelDBDumper.exe
+export GOARCH="386"
+go build -o LevelDBDumper_x86.exe
+zip -q LevelDBDumper.app.zip LevelDBDumper.app
