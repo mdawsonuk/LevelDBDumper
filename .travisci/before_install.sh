2def52f7167b4c91ae24c808b12456141517ccfe
Matt <matthewd102@hotmail.com>
2023-06-02 13:19:05 +0100

    Remove Travis CI files

--- .travisci/before_install.sh
+++
@@ -1,9 +0,0 @@
-chmod +x ./.travisci/test.sh
-chmod +x ./.travisci/deploy.sh
-export GO111MODULE=auto
-go get github.com/syndtr/goleveldb/leveldb
-go get github.com/hashicorp/go-version
-go get github.com/gookit/color
-if [ "$TRAVIS_OS_NAME" = "windows" ]; then go get golang.org/x/sys/windows; fi
-
-go get github.com/securego/gosec/cmd/gosec
--- .travisci/coverage.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd ..\src\LevelDBDumper
-go test -coverprofile=coverage.out
-go tool cover -func=coverage.out
-go tool cover -html=coverage.out
-del coverage.out
-pause
